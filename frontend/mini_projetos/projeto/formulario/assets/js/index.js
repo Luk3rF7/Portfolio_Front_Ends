0da4c8a4a71d13e431fc6fc91b2adcc9f97c7b5d
Luk3rf7 <Lkzeron@gmail.com>
2023-09-20 23:23:16 -0300

    feat: update

--- frontend/mini_projetos/projeto/formulario/assets/js/index.js
+++ frontend/mini_projetos/projeto/formulario/assets/js/index.js
@@ -1,23 +1,102 @@
 /* formulario */
-const slidePage = document.querySelector(".slidepage")
-const firstNextBtn = document.querySelector(".nextBtn")
+const slidePage = document.querySelector(".slidepage");
+const firstNextBtn = document.querySelector(".nextBtn");
 /* btn next & prev */
-const prevBtnSec = document.querySelector(".prev-1")
-const nextBtnSec = document.querySelector(".next-1")
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
 /* page 2*/
-const prevBtnThird = document.querySelector(".prev-2")
-const nextBtnThird = document.querySelector(".next-2")
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
 /* page 4 */
-const prevBtnSec = document.querySelector(".prev-3")
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
 /* submit */
-const submit = document.querySelector('.submit')
-firstNextBtn.addEventListener('click', function (e) {
-  e.preventDefault()
+const submit = document.querySelector('.submit');
+// step
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+const msgConcluido = document.querySelector(".msg")
+let max = 4;
+let current = 1;
+
+
+// evento manipulação:
+
+// proxima page
+firstNextBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
   slidePage.style.marginLeft = "-25%";
-  console.log('clicou')
+  //step 
+  bullet[current - 1].classList.add("active")
+  progressText[current - 1].classList.add("active")
+  progressCheck[current - 1].classList.add("active")
+  current += 1;
+})
+nextBtnSec.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  slidePage.style.marginLeft = "-50%";
+  //step 
+  bullet[current - 1].classList.add("active")
+  progressText[current - 1].classList.add("active")
+  progressCheck[current - 1].classList.add("active")
+  current += 1;
+})
+nextBtnThird.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  slidePage.style.marginLeft = "-75%";
+  //step 
+  bullet[current - 1].classList.add("active")
+  progressText[current - 1].classList.add("active")
+  progressCheck[current - 1].classList.add("active")
+  current += 1;
+})
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  //step 
+  bullet[current - 1].classList.add("active")
+  progressText[current - 1].classList.add("active")
+  progressCheck[current - 1].classList.add("active")
+  current += 1;
+  //timeout delay
+  msgConcluido.classList.add("active");
 })
 
+// anterior
+prevBtnSec.addEventListener("click", (e) => {
+  e.preventDefault();
 
+  slidePage.style.marginLeft = "0%";
+  //step 
+  bullet[current - 2].classList.remove("active")
+  progressText[current - 2].classList.remove("active")
+  progressCheck[current - 2].classList.remove("active")
+  current -= 1;
+
+})
+prevBtnThird.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  slidePage.style.marginLeft = "-25%";
+  //step 
+  bullet[current - 2].classList.remove("active")
+  progressText[current - 2].classList.remove("active")
+  progressCheck[current - 2].classList.remove("active")
+  current -= 1;
+})
+prevBtnFourth.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  slidePage.style.marginLeft = "-50%";
+  //step 
+  bullet[current - 2].classList.remove("active")
+  progressText[current - 2].classList.remove("active")
+  progressCheck[current - 2].classList.remove("active")
+  current -= 1;
+})
 /* side  */
 const dropOpen = document.querySelector('.web-btn')
 const webShow = document.querySelector('.web-show')
--- frontend/mini_projetos/projeto/lista_tarefa/assets/js/script.js
+++ frontend/mini_projetos/projeto/lista_tarefa/assets/js/script.js
@@ -1,12 +1,18 @@
-// seleção do elemento
+// * seleção do elemento
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
-//funçao
-const saveTodo = (text) => {
+const searchInput = document.querySelector('#search-input');
+const eraseBtn = document.querySelector('#erase-button');
+const filterBtn = document.querySelector('#filter-select');
+
+let oldInputValue;
+// * Funçao
+const saveTodo = (text, done = 0, save = 1) => {
+  /* criar */
   const todo = document.createElement("div");
   todo.classList.add('todo');
 
@@ -18,27 +24,249 @@ const saveTodo = (text) => {
   const doneBtn = document.createElement("button")
   doneBtn.classList.add("finish-todo")
   doneBtn.innerHTML = `<i class="fa-solid fa-check"></i>`
-  todo.appendChild(doneBtn)
+  todo.appendChild(doneBtn);
+
   /* editar*/
   const editBtn = document.createElement("button")
   editBtn.classList.add("edit-todo")
   editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
-  todo.appendChild(editBtn)
-  /* deletar */
+  todo.appendChild(editBtn);
 
+  /* deletar */
   const deleteBtn = document.createElement("button")
   deleteBtn.classList.add("remove-todo")
   deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
-  todo.appendChild(deleteBtn)
-  /*  */
+  todo.appendChild(deleteBtn);
+
+  /* localstorage  */
+  if (done) {
+    todo.classList.add("done");
+  }
+
+  if (save) {
+    saveTodoLocalStorage({ text, done: 0 });
+  }
+
+
+  /* retorn vazio */
   todoList.appendChild(todo);
+  todoInput.value = ""
+  todoInput.focus();
 }
-// Eventos
+
+//aparecer e desaparecer form
+const toggleForms = () => {
+  editForm.classList.toggle("hide")
+  todoForm.classList.toggle("hide")
+  todoList.classList.toggle("hide")
+}
+
+
+const updateTodo = (text) => {
+
+  const todos = document.querySelectorAll(".todo");
+
+  todos.forEach((todo) => {
+    //armazena titulo
+    let todoTitle = todo.querySelector("h3");
+    console.log(todoTitle, text)
+    //verifica o titulo 
+    if (todoTitle.innerText === oldInputValue) {
+      //passa para novo titulo
+      todoTitle.innerText = text;
+
+      //local storage 
+      updateTodoLocalStorage(oldInputValue, text);
+    }
+  });
+
+}
+//metodo buscar
+const getSearchedTodo = (search) => {
+  const todos = document.querySelectorAll(".todo");
+
+  todos.forEach((todo) => {
+    const todoTitle = todo.querySelector("h3").innerText.toLowerCase();
+
+    todo.style.display = "flex";
+
+    if (!todoTitle.includes(search)) {
+      todo.style.display = "none"
+    }
+  })
+}
+//metodo filtrar
+const filterTodos = (filterValue) => {
+  const todos = document.querySelectorAll(".todo");
+
+  switch (filterValue) {
+    // filtrar todos
+    case "all":
+      todos.forEach((todo) =>
+        (todo.style.display = "flex"));
+      break;
+    //filtar feitos
+    case "done":
+      todos.forEach((todo) => {
+        todo.classList.contains("done")
+          ? (todo.style.display = "flex")
+          : (todo.style.display = "none")
+      });
+      break;
+    //a fazer
+    case "todos":
+      todos.forEach((todo) => {
+        !todo.classList.contains("done")
+          ? (todo.style.display = "flex")
+          : (todo.style.display = "none")
+      }
+      );
+      break;
+
+    default:
+      break;
+  }
+}
+
+// * Eventos
+/*  envio form */
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
   const inputValue = todoInput.value;
+
   if (inputValue) {
     //save tarefa 
     saveTodo(inputValue)
   }
+});
+
+/* evento click */
+document.addEventListener('click', (e) => {
+
+  const targetEl = e.target;
+  const paramEl = targetEl.closest("div");
+  let todoTitle;
+
+  if (paramEl && paramEl.querySelector("h3")) {
+    todoTitle = paramEl.querySelector("h3").innerText || "";
+  }
+
+  // conclui tarefa
+  if (targetEl.classList.contains("finish-todo")) {
+    paramEl.classList.toggle("done");
+    //local Storage
+    updateTodoStatusLocalStorage(todoTitle);
+  }
+
+  // editar tarefa
+  if (targetEl.classList.contains("edit-todo")) {
+    toggleForms();
+
+    editInput.value = todoTitle;
+    oldInputValue = todoTitle;
+  }
+
+  //remover tarefa
+  if (targetEl.classList.contains("remove-todo")) {
+    paramEl.remove();
+    //localstorage :
+    removeTodoLocalStorage(todoTitle);
+  }
+  // cancelar
+  cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleForms();
+  })
+  //editar
+  editForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const editInputValue = editInput.value;
+
+    if (editInputValue) {
+      // atualizar
+      updateTodo(editInputValue);
+    }
+
+    toggleForms();
+  })
+
+  // Pesquisar :
+  searchInput.addEventListener("keyup", (e) => {
+    const search = e.target.value;
+    getSearchedTodo(search);
+
+  })
+  // limpa search 
+  eraseBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    searchInput.value = ""
+
+    search.dispatchEvent(new Event("keyup"));
+  })
+
+  //filtrar
+  filterBtn.addEventListener("change", (e) => {
+    const filterValue = e.target.value;
+    filterTodos(filterValue)
+    console.log(filterValue)
+  })
 })
+
+// local storage setting :
+//converter json
+const getTodosLocalStorage = () => {
+  const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  return todos;
+}
+//carregar
+const loadTodos = () => {
+  const todos = getTodosLocalStorage();
+
+  todos.forEach((todo) => {
+    saveTodo(todo.text, todo.done, 0);
+  });
+};
+//salva dados
+const saveTodoLocalStorage = (todo) => {
+  const todos = getTodosLocalStorage();
+
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+// remove dados do storage
+
+const removeTodoLocalStorage = (todoText) => {
+  const todos = getTodosLocalStorage();
+
+  // filtrar o array
+  const filteredTodos = todos.filter((todo) =>
+    todo.text != todoText
+  );
+
+  localStorage.setItem("todos", JSON.stringify(filteredTodos));
+};
+
+// atualizar Storage status
+
+const updateTodoStatusLocalStorage = (todoText) => {
+  const todos = getTodosLocalStorage();
+
+  todos.map((todo) =>
+    todo.text === todoText ? (todo.done = !todo.done) : null
+  )
+};
+
+const updateTodoLocalStorage = (todoOldText, todoNewText) => {
+  const todos = getTodosLocalStorage();
+
+  todos.map((todo) =>
+    todo.text === todoOldText ? (todo.text = todoNewText) : null
+  );
+
+  localStorage.setItem("todos", JSON.stringify(todos))
+};
+
+loadTodos();
